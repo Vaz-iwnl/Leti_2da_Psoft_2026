a5f91113d2e7cc4036836b7e7865770a306d27f5
pcvaz <1240908@isep.ipp.pt>
2026-05-20 10:22:22 +0100

    Revert "correção do DDD"

--- demo/src/main/java/com/aisafe/repository/AircraftModelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aisafe.repository;
-
-import com.aisafe.model.AircraftModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AircraftModelRepository extends JpaRepository<AircraftModel, String> {
-}
